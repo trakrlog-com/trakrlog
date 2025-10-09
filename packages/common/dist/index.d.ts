b1f867cb8375ed1c2f9aba91c70e66e217c63034
dev-bre <anbrandi@gmail.com>
2025-10-09 18:12:37 +0100

    Reset repo with proper .gitignore

--- packages/common/dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export interface User {
-    id: string;
-    name: string;
-    email: string;
-}
-export declare function isValidEmail(email: string): boolean;
--- packages/common/dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isValidEmail = isValidEmail;
-function isValidEmail(email) {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-}
